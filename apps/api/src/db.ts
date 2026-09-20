f3a9b78fb755066743797abca3cbdc971321170c
sonudhonchak <sonudhonchak00@gmail.com>
2026-09-20 12:56:55 +0530

    Update db.ts

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../../generated/prisma/client";
+import { PrismaClient } from "../../../generated/prisma/client.js";
 
 const connectionString = process.env.DATABASE_URL;
 
